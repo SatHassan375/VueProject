b4d682981c4dd174b3f7ea011edaf8d6f17d2766
Syed Hassan Raza <105318011+HassanRaza375@users.noreply.github.com>
2024-11-06 00:27:13 +0500

    vuetify added

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-container>
+    <v-row>
+      <v-col xl="12" md="6">
+        <a href="https://vite.dev" target="_blank">
+          <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://vuejs.org/" target="_blank">
+          <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+        </a>
+      </v-col>
+      <v-col xl="12" md="12">
+        <HelloWorld msg="Vite + Vue" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
