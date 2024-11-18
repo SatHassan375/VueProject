7a83bb15dcf9190a55430b9510dd1399c9263ad6
Syed Hassan Raza <hassanraza@satjapan.co>
2024-11-18 19:03:51 +0500

    fdsdfsd

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,29 @@
 <script setup>
 import { ref } from "vue";
-
 defineProps({
   msg: String,
 });
 
-const count = ref(0);
+let count = ref(0);
+
+const fun = () => {
+  count.value += 1;
+  arr.push(count)
+}
+const arr = [1, 2, 3, 4, 5]
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
-    <v-card class="pa-4">
-      <div class="custom-padding-card">
-        <v-btn color="primary" @click="count++"> Button {{ count }} </v-btn>
-      </div>
-    </v-card>
-
+  <v-card class="pa-4">
+    <div class="custom-padding-card">
+      <v-btn color="primary" @click="fun"> Button {{ count }} </v-btn>
+    </div>
+    <ul>
+      <li v-for="item in arr">{{ item }}</li>
+    </ul>
+  </v-card>
 </template>
 
 <style scoped>
