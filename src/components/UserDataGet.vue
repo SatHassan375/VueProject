09d70efd47d2c80832b430fabcf0db520c7c0b34
Syed Hassan Raza <hassanraza@satjapan.co>
2024-11-20 19:00:53 +0500

    updates

--- src/components/UserDataGet.vue
+++ src/components/UserDataGet.vue
@@ -17,11 +17,16 @@ const funData = () => {
 <template>
     {{ formData }}
     <v-sheet class="d-flex flex-wrap bg-surface-variant">
-        <template v-for="item in inputData">
+        <template v-for="(item, i) in inputData">
             <v-sheet class="ma-2 pa-2 flex-1-1-100">
-                <v-text-field v-model="formData[item.inputName]" :type="item.type" :label="item.label"></v-text-field>
+                <v-text-field :key="i" v-model="formData[item.inputName]" variant="outlined" :type="item.type"
+                    :label="item.label"></v-text-field>
             </v-sheet>
         </template>
+        <v-sheet class="ma-2 pa-2 flex-1-1-100">
+            <v-select v-model="formData.selection" label="Select" variant="outlined"
+                :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"></v-select>
+        </v-sheet>
         <v-sheet class="ma-2 pa-2 flex-1-1-100">
             <v-btn color="indigo-darken-3" variant="flat" block @click="funData">Sumbit</v-btn>
         </v-sheet>
